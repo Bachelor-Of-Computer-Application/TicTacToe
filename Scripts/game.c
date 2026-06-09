92ead492f7b8950f91174083176b48316d1a1c99
aabishkaracharya7-wq <aabishkaracharya7@gmail.com>
2026-06-09 17:32:58 +0545

    updated game.c

--- Scripts/game.c
+++ Scripts/game.c
@@ -21,5 +21,31 @@ void InitGame()
     gameOver = 0;
     winner = ' ';
 }
+void ResetGame()
+{
+    InitGame();
+}
+
+
+void SaveMatch(char w)
+{
+    FILE *f = fopen("stats.txt", "a+");
+    if (!f) return;
+
+    // Count existing games
+    int count = 0;
+    char line[100];
+
+    rewind(f);
+    while (fgets(line, sizeof(line), f))
+    {
+        if (strncmp(line, "Game", 4) == 0)
+            count++;
+    }
+
+    fprintf(f, "Game %d: %c\n", count + 1, w);
+
+    fclose(f);
+}
 
 
