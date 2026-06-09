7630994a9198026fbc24900d1594881e3366d315
aabishkaracharya7-wq <aabishkaracharya7@gmail.com>
2026-06-09 17:36:21 +0545

    updated game.c 2nd time

--- Scripts/game.c
+++ Scripts/game.c
@@ -47,5 +47,34 @@ void SaveMatch(char w)
 
     fclose(f);
 }
+int CheckWin()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] != ' ' &&
+            board[i][0] == board[i][1] &&
+            board[i][1] == board[i][2])
+            return 1;
+    }
 
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] != ' ' &&
+            board[0][i] == board[1][i] &&
+            board[1][i] == board[2][i])
+            return 1;
+    }
+
+    if (board[0][0] != ' ' &&
+        board[0][0] == board[1][1] &&
+        board[1][1] == board[2][2])
+        return 1;
+
+    if (board[0][2] != ' ' &&
+        board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0])
+        return 1;
+
+    return 0;
+}
 
