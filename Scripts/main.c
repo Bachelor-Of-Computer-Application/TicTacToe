15d4d82d7297380d69a22aafe67695fa432a0fde
Prakat07 <prakatbaral8@gmail.com>
2026-06-16 13:18:32 +0545

    uptated code

--- Scripts/main.c
+++ Scripts/main.c
@@ -50,3 +50,93 @@ void SaveUser(char *u, char *p)
     }
 }
 
+// ---------------- MAIN ----------------
+int main()
+{
+    InitWindow(800, 600, "TicTacToe");
+    SetTargetFPS(60);
+
+    FILE *fcheck = fopen("users.txt", "r");
+    if (!fcheck)
+        screen = 1;
+    else
+    {
+        screen = 2;
+        fclose(fcheck);
+    }
+
+    InitGame();
+
+    while (!WindowShouldClose())
+    {
+        Vector2 m = GetMousePosition();
+
+        // ---------------- TEXT INPUT ----------------
+        int key = GetCharPressed();
+        while (key > 0)
+        {
+            if (typingUsername && usernameLen < 49)
+            {
+                username[usernameLen++] = (char)key;
+                username[usernameLen] = '\0';
+            }
+
+            if (typingPassword && passwordLen < 49)
+            {
+                password[passwordLen++] = (char)key;
+                password[passwordLen] = '\0';
+            }
+
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE))
+        {
+            if (typingUsername && usernameLen > 0)
+                username[--usernameLen] = '\0';
+
+            if (typingPassword && passwordLen > 0)
+                password[--passwordLen] = '\0';
+        }
+
+        // ---------------- MOUSE ----------------
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            typingUsername = 0;
+            typingPassword = 0;
+
+            // ================= GAME SCREEN =================
+            if (screen == 3)
+            {
+                // STATS BUTTON CLICK
+                if (m.x >= 10 && m.x <= 130 &&
+                    m.y >= 10 && m.y <= 50)
+                {
+                    showStats = !showStats;
+                }
+
+                HandleGameClick(m.x, m.y);
+            }
+
+            // ================= SIGNUP =================
+            if (screen == 1)
+            {
+                loginError = 0;
+
+                if (m.x >= 250 && m.x <= 550 &&
+                    m.y >= 200 && m.y <= 240)
+                    typingUsername = 1;
+
+                if (m.x >= 250 && m.x <= 550 &&
+                    m.y >= 260 && m.y <= 300)
+                    typingPassword = 1;
+
+                if (m.x >= 250 && m.x <= 550 &&
+                    m.y >= 340 && m.y <= 390)
+                {
+                    SaveUser(username, password);
+                    screen = 2;
+                }
+            }
+
+            
